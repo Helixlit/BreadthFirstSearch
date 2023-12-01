6c9da1c38f3dd0e8956ea7a9e36577c8a6460865
Helixlit <felix.burgthaler@gmx.de>
2023-12-01 11:15:15 +0100

    af

---
+++ src/Graph.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+
+    private List<Edge> edges;
+    private List<Vertex> vertices;
+
+    private int[][] matrix;
+
+    public Graph() {
+        this.edges = new ArrayList();
+    }
+
+    //TODO
+    public void addVertex() {}
+
+    //TODO
+    public void addEdge() {}
+
+    //TODO
+    public void removeVertex() {}
+
+    //TODO
+    public void removeEdge() {}
+
+    //TODO
+    public void getVertexValue() {}
+
+    //TODO
+    public void setVertexValue() {}
+
+    //TODO
+    public void getEdgeValue() {}
+
+    //TODO
+    public void setEdgeValue() {}
+}
